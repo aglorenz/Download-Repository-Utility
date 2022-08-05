4c97ae1bbb3e28c16fde17f5365a379e8d398435
Andrew Lorenz <aglorenz777@gmail.com>
2022-08-04 21:01:52 -0700

    current development

--- Download.py
+++ Download.py
@@ -16,13 +16,20 @@ def run(cmd):
     completed = subprocess.run(["powershell.exe", "-ExecutionPolicy", "RemoteSigned", "-Command", cmd], capture_output=True)
     return completed
 
+def print_file(file_path):
+    f = open(file_path, 'r')
+    print(f.read())
+
+
 if __name__ == '__main__':
-    ps_command = "./download1.ps1"
-    result = run(ps_command)
-    if result.returncode != 0:
-        print("An error occured: %s", result.stderr)
-    else:
-        print("Download executed successfully!")
+    print_file('temp.txt')
+##    ps_command = "./download_repo.ps1 -repoName 'JavaScript-Projects' -user 'jefflicano82' | Out-File -Encoding ASCII temp.txt"
+##    result = run(ps_command)
+##    if result.returncode != 0:
+##        print("An error occured: %s", result.stderr)
+##    else:
+##        print("Download executed successfully!")
+##        print_file('temp.txt')
 
     
 
--- download_repo.ps1
+++ download_repo.ps1
@@ -1,44 +1,50 @@
 ######################################################################
 #                                                                    #
 # Download and Unzip GitHub Repository                               #
-# Author: Sandro Pereira                                             #
+# Author: Sandro Pereira https://blog.sandro-pereira.com/2020/09/01  #
+#          /how-to-download-a-github-repository-using-powershell/    #
+# Modified:                                                          #
+# 8/4/2022 Andrew Lorenz Tweaked for our needs   					 #
+#                        Prerequisites: Powershell v5.1				 #
 #                                                                    #
 ######################################################################
 
-# function DownloadGitHubRepository 
-# { 
-    # param( 
-       # [Parameter(Mandatory=$True)] 
-       # [string] $repoName, 
-        
-       # [Parameter(Mandatory=$True)] 
-       # [string] $author, 
-        
-       # [Parameter(Mandatory=$False)] 
-       # [string] $branch = "master", 
-        
-       # [Parameter(Mandatory=$False)] 
-       # [string] $destination = "c:\temp" 
-    # ) 
+param( 
+   [Parameter(Mandatory=$True)] 
+   [string] $repoName, 
 	
-$repoName = 'JavaScript-Projects'
-$author = 'Eli-gav'
-$branch = 'main'
-$destination = 'C:\Users\Andy\Documents\_Student Repos'
+   [Parameter(Mandatory=$True)] 
+   [string] $user, 
+	
+   [Parameter(Mandatory=$False)] 
+   [string] $branch = "main", 
+	
+   [Parameter(Mandatory=$False)] 
+   [string] $destination = "c:\temp" 
+) 
+	
+$repoName
+$user
+$branch
+$destination
 
+# $repoName = 'JavaScript-Projects'
+# $user = 'jefflicano82'
+# $branch = 'main'
+# $destination = 'C:\Users\Andy\Documents\_Student Repos'
 
 # Force to create a zip file 
-$zipFile = "$destination\$author-$repoName.zip" # prepend author name to differntiate other student's repos
+$zipFile = "$destination\$user-$repoName.zip" # prepend user name to differntiate other student's repos
 $zipFile
 
 New-Item $zipFile -ItemType File -Force  # | Out-Null # supress output with Out-Null
 
 # this is the same way as when you click on the green download button
-$repositoryZipUrl = "https://github.com/$author/$repoName/archive/$branch.zip"
+$repositoryZipUrl = "https://github.com/$user/$repoName/archive/$branch.zip"
 #$repositoryZipUrl = "https://github.com/sandroasp/Microsoft-Integration-and-Azure-Stencils-Pack-for-Visio/archive/master.zip"
 
 # using the API instead
-#$repositoryZipUrl = "https://api.github.com/repos/$author/$repoName/zipball/$branch"  
+#$repositoryZipUrl = "https://api.github.com/repos/$user/$repoName/zipball/$branch"  
 # download the zip 
 Write-Host 'Starting downloading the GitHub Repository'
 Invoke-RestMethod -Uri $repositoryZipUrl -OutFile $zipFile
